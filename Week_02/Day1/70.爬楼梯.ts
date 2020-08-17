f7a4c257f02b5673cd01d7a49041b3c088935a8a
xuke <blk1314@126.com>
2020-08-17 20:37:25 +0800

    使用滚动数组的方法

---
+++ Week_02/Day1/70.爬楼梯.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=typescript
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+    let p = 0;
+    let q = 0;
+    let r = 1;
+    for (let i = 1; i <= n; i++) {
+        p = q;
+        q = r;
+        r = p + q;
+    }
+    return r;
+};
+// @lc code=end
