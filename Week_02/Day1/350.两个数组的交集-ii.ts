07fb5a8014033d59ab5d9fd78495ab112bafadfa
xuke <blk1314@126.com>
2020-08-17 20:20:01 +0800

    使用map解题

--- Week_02/Day1/350.两个数组的交集-ii.ts
+++ Week_02/Day1/350.两个数组的交集-ii.ts
@@ -6,14 +6,31 @@
 
 // @lc code=start
 function intersect(nums1: number[], nums2: number[]): number[] {
+    if (nums1.length > nums2.length) {
+        return intersect(nums2, nums1);
+    }
+    let map = new Map();
+    for (let i = 0; i < nums1.length; i++) {
+        let key = nums1[i];
+        let val = 1;
+        if (map.has(key)) {
+            val = map.get(key);
+            map.set(key, val + 1);
+        } else {
+            map.set(key, val);
+        }
+    }
     let result: number[] = [];
-    let len = nums1.length;
-    for (let i = 0; i < len; i++) {
-        let val = nums1[i];
-        let nums2Index = nums2.indexOf(val);
-        if (nums2Index >= 0) {
-            result.push(val);
-            nums2.splice(nums2Index, 1)
+    for (let j = 0; j < nums2.length; j++) {
+        let key = nums2[j];
+        if (map.has(key)) {
+            result.push(key);
+            let count = map.get(key) - 1;
+            if (count > 0) {
+                map.set(key, count)
+            } else {
+                map.delete(key);
+            }
         }
     }
     return result;
