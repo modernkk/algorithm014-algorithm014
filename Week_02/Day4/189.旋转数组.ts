5e08530d3a1e973d9b928037f74471eb3ee1041e
xuke <blk1314@126.com>
2020-08-20 20:56:07 +0800

    [189] 旋转数组

--- Week_02/Day4/189.旋转数组.ts
+++ Week_02/Day4/189.旋转数组.ts
@@ -9,7 +9,20 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
 
 };
+function reverse(nums: number[], start: number, end: number) {
+    while (start < end) {
+        let temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
 // @lc code=end
 
