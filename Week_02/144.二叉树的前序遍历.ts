d66ed7c6d63941f481e3477aff506684ced1f780
xuke <blk1314@126.com>
2020-08-22 00:04:37 +0800

    二叉树的前序遍历

--- Week_02/144.二叉树的前序遍历.ts
+++ Week_02/144.二叉树的前序遍历.ts
@@ -5,22 +5,29 @@
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+// Definition for a binary tree node.
+// class TreeNode {
+//     val: number
+//     left: TreeNode | null
+//     right: TreeNode | null
+//     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//         this.val = (val === undefined ? 0 : val)
+//         this.left = (left === undefined ? null : left)
+//         this.right = (right === undefined ? null : right)
+//     }
+// }
 
 function preorderTraversal(root: TreeNode | null): number[] {
-
+    let result: number[] = [];
+    helper(root, result);
+    return result;
 };
+function helper(root: TreeNode | null, ans: number[]) {
+    if (root !== null) {
+        ans.push(root.val);
+        helper(root.left, ans);
+        helper(root.right, ans);
+    }
+}
 // @lc code=end
 
