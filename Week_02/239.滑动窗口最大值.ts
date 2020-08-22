e45ccee0c27e812cbbe9a114605530039f37f298
xuke <blk1314@126.com>
2020-08-23 00:01:29 +0800

    [239] 滑动窗口最大值

--- Week_02/239.滑动窗口最大值.ts
+++ Week_02/239.滑动窗口最大值.ts
@@ -6,7 +6,16 @@
 
 // @lc code=start
 function maxSlidingWindow(nums: number[], k: number): number[] {
-
+    let result: number[] = [];
+    for (let i = 0; i < nums.length - k + 1; i++) {
+        let max = nums[i];
+        for (let j = i; j < k + i; j++) {
+            let temp = nums[j];
+            max = Math.max(max, temp);
+        }
+        result.push(max);
+    }
+    return result;
 };
 // @lc code=end
 
