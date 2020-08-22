87ff21dbf59debea1710de076f5a3434bfdfa8e5
xuke <blk1314@126.com>
2020-08-22 22:33:10 +0800

    [22] 括号生成

--- Week_02/Day6/22.括号生成.ts
+++ Week_02/Day6/22.括号生成.ts
@@ -6,7 +6,28 @@
 
 // @lc code=start
 function generateParenthesis(n: number): string[] {
-
+    let ans: string[] = new Array();
+    if (n === 0) {
+        return ans;
+    }
+    dfs('', n, n, ans);
+    return ans;
 };
-// @lc code=end
+function dfs(curStr: string, left: number, right: number, ans: string[]) {
+    if (left === 0 && right === 0) {
+        ans.push(curStr);
+        return;
+    }
 
+    if (left > right) {
+        return;
+    }
+
+    if (left > 0) {
+        dfs(`${curStr}(`, left - 1, right, ans);
+    }
+    if (right > 0) {
+        dfs(`${curStr})`, left, right - 1, ans);
+    }
+}
+// @lc code=end
