45a4f1b13a99b485203ce179873779d5b2c0c745
xuke <blk1314@126.com>
2020-08-22 23:52:50 +0800

    [49] 字母异位词分组

--- Week_02/49.字母异位词分组.ts
+++ Week_02/49.字母异位词分组.ts
@@ -6,7 +6,31 @@
 
 // @lc code=start
 function groupAnagrams(strs: string[]): string[][] {
+    let map = new Map();
+    let charCodeA = 'a'.charCodeAt(0);
+    for (let i = 0; i < strs.length; i++) {
+        let num = new Array(26);
+        for (let j = 0; j < num.length; j++) {
+            num[j] = 0;
+        }
 
+        for (let k = 0; k < strs[i].length; k++) {
+            num[strs[i].charCodeAt(k) - charCodeA]++;
+        }
+        let key = num.join('#');
+        if (map.has(key)) {
+            let hasVal = map.get(key)
+            hasVal.push(strs[i]);
+            map.set(key, hasVal);
+        } else {
+            map.set(key, [strs[i]])
+        }
+    }
+    let result = [];
+    for (let val of map.values()) {
+        result.push(val);
+    }
+    return result;
 };
 // @lc code=end
 
