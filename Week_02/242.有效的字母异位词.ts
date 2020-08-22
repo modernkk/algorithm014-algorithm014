6ba2964bae63afbf92721fac27c14aaaadc5a88b
xuke <blk1314@126.com>
2020-08-22 23:13:10 +0800

    [242] 有效的字母异位词

--- Week_02/242.有效的字母异位词.ts
+++ Week_02/242.有效的字母异位词.ts
@@ -6,7 +6,32 @@
 
 // @lc code=start
 function isAnagram(s: string, t: string): boolean {
-
+    if (s.length !== t.length) {
+        return false;
+    }
+    let map = new Map();
+    for (let i = 0; i < s.length; i++) {
+        let key = s[i];
+        let count = 1;
+        if (map.has(key)) {
+            count = map.get(key) + 1;
+        }
+        map.set(key, count)
+    }
+    for (let j = 0; j < t.length; j++) {
+        let key = t[j];
+        if (!map.has(key)) {
+            return false;
+        }
+        let count = map.get(key);
+        map.set(key, --count);
+    }
+    for (let val of map.values()) {
+        if (val !== 0) {
+            return false;
+        }
+    }
+    return true;
 };
 // @lc code=end
 
