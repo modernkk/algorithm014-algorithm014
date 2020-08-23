a587fc2039c7f0587c4605823a7240de8e524bc0
xuke <blk1314@126.com>
2020-08-23 16:12:22 +0800

    [347] 前 K 个高频元素

--- Week_02/347.前-k-个高频元素.ts
+++ Week_02/347.前-k-个高频元素.ts
@@ -6,7 +6,19 @@
 
 // @lc code=start
 function topKFrequent(nums: number[], k: number): number[] {
-
+    nums.sort((a, b) => a - b);
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let key = nums[i];
+        let count = 1;
+        if (map.has(key)) {
+            count += map.get(key);
+        }
+        map.set(key, count)
+    }
+    let result = [...new Set(nums)];
+    result.sort((a, b) => map.get(b) - map.get(a));
+    return result.slice(0, k);
 };
 // @lc code=end
 
