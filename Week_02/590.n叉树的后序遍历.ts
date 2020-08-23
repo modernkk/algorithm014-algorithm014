55032abeb4bf6578ced74a21ae155d53f83e70f7
xuke <blk1314@126.com>
2020-08-23 16:36:45 +0800

    590.n叉树的后序遍历

--- Week_02/590.n叉树的后序遍历.ts
+++ Week_02/590.n叉树的后序遍历.ts
@@ -10,6 +10,19 @@
  * @param {Node} root
  * @return {number[]}
  */
-var postorder = function(root) {
-    
+var postorder = function (root) {
+    let result = [];
+    helper(root, result);
+    return result;
 };
+
+function helper(node, ans) {
+    if (node !== null) {
+        for (let child of node.children) {
+            if (child !== null) {
+                helper(child, ans)
+            }
+        }
+        ans.push(node.val);
+    }
+}
