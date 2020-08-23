004713daf0f9681a550bc307b95354a872486b4f
xuke <blk1314@126.com>
2020-08-23 16:20:27 +0800

    [263] 丑数

--- Week_02/263.丑数.ts
+++ Week_02/263.丑数.ts
@@ -6,7 +6,22 @@
 
 // @lc code=start
 function isUgly(num: number): boolean {
-
+    if (num < 1) {
+        return false;
+    }
+    if (num === 1) {
+        return true;
+    }
+    if (num % 2 === 0) {
+        num /= 2;
+    } else if (num % 3 === 0) {
+        num /= 3;
+    } else if (num % 5 === 0) {
+        num /= 5;
+    } else {
+        return false;
+    }
+    return isUgly(num);
 };
 // @lc code=end
 
