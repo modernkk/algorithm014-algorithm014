092021221e1254b8a538149d15746f27867420a9
xuke <blk1314@126.com>
2020-08-23 16:30:40 +0800

    589.n叉树的前序遍历

--- Week_02/589.n叉树的前序遍历.ts
+++ Week_02/589.n叉树的前序遍历.ts
@@ -10,6 +10,17 @@
  * @param {Node} root
  * @return {number[]}
  */
-var preorder = function(root) {
-    
+let preorder = function(root) {
+    let result = [];
+    dfs(root, result);
+    return result;
 };
+
+function dfs(node, ans) {
+    if (node !== null) {
+        ans.push(node.val);
+        for (let child of node.children) {
+            dfs(child, ans)
+        }
+    }
+}
