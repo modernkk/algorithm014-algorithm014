d804fbb4a57235fef2538e96f4ac8563960aa6fd
xuke <blk1314@126.com>
2020-08-23 17:09:04 +0800

    429.n叉树的层序遍历

--- Week_02/429.n叉树的层序遍历.ts
+++ Week_02/429.n叉树的层序遍历.ts
@@ -10,6 +10,23 @@
  * @param {Node} root
  * @return {number[][]}
  */
-var levelOrder = function(root) {
-    
+var levelOrder = function (root) {
+    if (root === null) {
+        return [];
+    }
+    let result = [];
+    let queue = [root];
+    while (queue.length > 0) {
+        let level = [];
+        let size = queue.length;
+        for (let i = 0; i < size; i++) {
+            let node = queue.shift();
+            level.push(node.val);
+            for (let c of node.children) {
+                queue.push(c)
+            }
+        }
+        result.push(level);
+    }
+    return result;
 };
