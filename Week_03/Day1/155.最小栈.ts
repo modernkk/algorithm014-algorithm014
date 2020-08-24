5b316a5526960e9875e9c3d7c6eeca7e9e66530c
xuke <blk1314@126.com>
2020-08-24 23:53:53 +0800

    [155] 最小栈

--- Week_03/Day1/155.最小栈.ts
+++ Week_03/Day1/155.最小栈.ts
@@ -6,24 +6,29 @@
 
 // @lc code=start
 class MinStack {
+    stack: number[];
+    min_stack: number[];
     constructor() {
-
+        this.stack = [];
+        this.min_stack = [Infinity];
     }
 
     push(x: number): void {
-
+        this.stack.push(x);
+        this.min_stack.push(Math.min(x, this.min_stack[this.min_stack.length - 1]));
     }
 
     pop(): void {
-
+        this.stack.pop();
+        this.min_stack.pop();
     }
 
     top(): number {
-
+        return this.stack[this.stack.length - 1];
     }
 
     getMin(): number {
-
+        return this.min_stack[this.min_stack.length - 1];
     }
 }
 
