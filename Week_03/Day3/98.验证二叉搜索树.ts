32b84741ee7ef35b6b27b51fcdc454b97bec8d67
xuke <blk1314@126.com>
2020-08-26 20:27:26 +0800

    [98] 验证二叉搜索树

--- Week_03/Day3/98.验证二叉搜索树.ts
+++ Week_03/Day3/98.验证二叉搜索树.ts
@@ -20,7 +20,17 @@
  */
 
 function isValidBST(root: TreeNode | null): boolean {
-
+    return helper(root, -Infinity, Infinity);
 };
+function helper(root: TreeNode | null, lower: number, upper: number): boolean {
+    if (root === null) {
+        return true;
+    }
+    if (root.val <= lower || root.val >= upper) {
+        return false;
+    }
+
+    return helper(root.left, lower, root.val) && helper(root.right, root.val, upper);
+}
 // @lc code=end
 
