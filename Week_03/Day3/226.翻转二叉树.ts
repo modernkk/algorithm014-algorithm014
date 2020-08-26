34e0b7c11d6aca804c7f5c6491a500e05e1860a5
xuke <blk1314@126.com>
2020-08-26 20:53:19 +0800

    [226] 翻转二叉树

--- Week_03/Day3/226.翻转二叉树.ts
+++ Week_03/Day3/226.翻转二叉树.ts
@@ -20,7 +20,14 @@
  */
 
 function invertTree(root: TreeNode | null): TreeNode | null {
-
+    if (root === null) {
+        return root;
+    }
+    let left = invertTree(root.left);
+    let right = invertTree(root.right);
+    root.right = left;
+    root.left = right;
+    return root;
 };
 // @lc code=end
 
