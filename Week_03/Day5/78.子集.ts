194bc63d946213983f4b8a447ce581cf28ee74fb
xuke <blk1314@126.com>
2020-08-28 20:44:44 +0800

    [78] 子集

--- Week_03/Day5/78.子集.ts
+++ Week_03/Day5/78.子集.ts
@@ -6,7 +6,20 @@
 
 // @lc code=start
 function subsets(nums: number[]): number[][] {
-
+    let output: number[][] = [];
+    output.push([]);
+    for (let num of nums) {
+        let subsets = [];
+        for (let o of output) {
+            let s = new Set(o);
+            s.add(num);
+            subsets.push(s);
+        }
+        for (let sub of subsets) {
+            output.push([...sub]);
+        }
+    }
+    return output;
 };
 // @lc code=end
 
