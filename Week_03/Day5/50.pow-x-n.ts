c9dfb47148815b43640b8032289e11b9ac56624f
xuke <blk1314@126.com>
2020-08-28 22:44:37 +0800

    [50] Pow(x, n)

--- Week_03/Day5/50.pow-x-n.ts
+++ Week_03/Day5/50.pow-x-n.ts
@@ -6,7 +6,19 @@
 
 // @lc code=start
 function myPow(x: number, n: number): number {
-
+    let result = qucMul(x, Math.abs(n));
+    if (n > 0) {
+        return result
+    } else {
+        return 1 / result;
+    }
 };
+function qucMul(x: number, n: number): number {
+    if (n === 0) {
+        return 1;
+    }
+    let y = qucMul(x, Math.floor(n / 2));
+    return n % 2 === 0 ? y * y: y * y * x;
+} 
 // @lc code=end
 
