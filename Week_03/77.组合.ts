22f4140464715fe38be342efa651c7b380dbd25d
xuke <blk1314@126.com>
2020-08-30 21:13:38 +0800

    [77] 组合

--- Week_03/77.组合.ts
+++ Week_03/77.组合.ts
@@ -6,7 +6,21 @@
 
 // @lc code=start
 function combine(n: number, k: number): number[][] {
-
+    let output: number[][] = [];
+    let curr: number[] = []
+    function backtrack(first: number, curr: number[]) {
+        if (curr.length === k) {
+            output.push(curr.slice(0))
+            return;
+        }
+        for (let i = first; i < n + 1; i++) {
+            curr.push(i);
+            backtrack(i + 1, curr);
+            curr.pop()
+        }
+    }
+    backtrack(1, curr);
+    return output;
 };
 // @lc code=end
 
