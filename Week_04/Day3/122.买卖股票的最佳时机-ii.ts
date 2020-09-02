b6804ea24e21aaf3a47e9a65441b83b17095d0a1
xuke <blk1314@126.com>
2020-09-02 19:43:00 +0800

    [122] 买卖股票的最佳时机 II

--- Week_04/Day3/122.买卖股票的最佳时机-ii.ts
+++ Week_04/Day3/122.买卖股票的最佳时机-ii.ts
@@ -6,7 +6,13 @@
 
 // @lc code=start
 function maxProfit(prices: number[]): number {
-
+    let max = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            max += prices[i] - prices[i - 1];
+        }
+    }
+    return max;
 };
 // @lc code=end
 
