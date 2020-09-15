5088b395ddcee177afce23e14f594d840ba854a9
xuke <blk1314@126.com>
2020-09-15 20:47:38 +0800

    [322] 零钱兑换

--- Week_06/322.零钱兑换.ts
+++ Week_06/322.零钱兑换.ts
@@ -6,7 +6,26 @@
 
 // @lc code=start
 function coinChange(coins: number[], amount: number): number {
-
+    if (coins.length === 0) {
+        return -1;
+    }
+    coins.sort((a, b) => b - a);
+    let ans = Infinity;
+    function helper(coins: number[], amount: number, currentIdx: number, count: number) {
+        if (amount === 0) {
+            ans = Math.min(ans, count);
+            return;
+        }
+        if (currentIdx >= coins.length) {
+            return;
+        }
+        for (let k = Math.floor(amount / coins[currentIdx]); k >= 0 && k + count < ans ; k--) {
+            helper(coins, amount - k * coins[currentIdx], currentIdx + 1, k + count);
+        }
+    }
+    helper(coins, amount, 0, 0);
+    return ans === Infinity ? -1 : ans;
 };
+export {};
 // @lc code=end
 
