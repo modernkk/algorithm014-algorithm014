af56f404297e7d7a91d47d67f4bb8fa269777fa5
xuke <blk1314@126.com>
2020-10-13 17:36:41 +0800

    [151] 翻转字符串里的单词

--- Week_09/Day2/151.翻转字符串里的单词.ts
+++ Week_09/Day2/151.翻转字符串里的单词.ts
@@ -6,7 +6,16 @@
 
 // @lc code=start
 function reverseWords(s: string): string {
-
+    let arr = s.split(' ');
+    let len = arr.length;
+    let result = '';
+    for (let i = len - 1; i >= 0; i--) {
+        let word = arr[i];
+        if (word.trim() !== '') {
+            result = result ? `${result} ${word}`: `${word}`;
+        }
+    }
+    return result;
 };
 // @lc code=end
 
