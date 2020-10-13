82f5a5c13fa4f5741d128b223e5f401bc6fcc36b
xuke <blk1314@126.com>
2020-10-13 19:46:27 +0800

    [557] 反转字符串中的单词 III

--- Week_09/Day2/557.反转字符串中的单词-iii.ts
+++ Week_09/Day2/557.反转字符串中的单词-iii.ts
@@ -6,7 +6,12 @@
 
 // @lc code=start
 function reverseWords(s: string): string {
-
+    let arrS = s.split(' ');
+    for (let i = 0; i < arrS.length; i++) {
+        let word = arrS[i];
+        arrS[i] = word.split('').reverse().join('');
+    }
+    return arrS.join(' ');
 };
 // @lc code=end
 
