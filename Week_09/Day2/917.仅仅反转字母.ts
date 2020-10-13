974b8ea6c20463791fdec8d99b4a518c45b646a5
xuke <blk1314@126.com>
2020-10-13 17:58:08 +0800

    [917] 仅仅反转字母

--- Week_09/Day2/917.仅仅反转字母.ts
+++ Week_09/Day2/917.仅仅反转字母.ts
@@ -6,7 +6,30 @@
 
 // @lc code=start
 function reverseOnlyLetters(S: string): string {
-
+    let arrS = S.split('');
+    let len = arrS.length;
+    let left = 0;
+    let right = len - 1;
+    let reg = /[a-zA-Z]/;
+    while (left < right) {
+        let vl = arrS[left];
+        let vr = arrS[right];
+        if (reg.test(vl) && reg.test(vr)) {
+            let tmp = vl;
+            arrS[left] = vr;
+            arrS[right] = tmp;
+            left++;
+            right--;
+        } else if (!reg.test(vl) && reg.test(vr)) {
+            left++;
+        } else if (reg.test(vl) && !reg.test(vr)) {
+            right--;
+        } else if (!reg.test(vl) && !reg.test(vr)) {
+            left++;
+            right--;
+        }
+    }
+    return arrS.join('');
 };
 // @lc code=end
 
