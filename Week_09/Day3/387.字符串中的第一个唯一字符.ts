aeb52bad542af223208749623fc29c1127cc007b
xuke <blk1314@126.com>
2020-10-14 16:30:53 +0800

    [387] 字符串中的第一个唯一字符

--- Week_09/Day3/387.字符串中的第一个唯一字符.ts
+++ Week_09/Day3/387.字符串中的第一个唯一字符.ts
@@ -6,7 +6,25 @@
 
 // @lc code=start
 function firstUniqChar(s: string): number {
-
+    let map = new Map<string, any>();
+    
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i];
+        if (map.has(c)) {
+            let v = map.get(c);
+            map.set(c, { count: v.count + 1, index: v.index });
+        } else {
+            map.set(c, { count: 1, index: i });
+        }
+    }
+    let res = -1;
+    for (let value of map.values()) {
+        let { count, index } = value;
+        if (count === 1) {
+            res = res === -1 ? index : Math.min(res, index);
+        }
+    }
+    return res;
 };
 // @lc code=end
 
