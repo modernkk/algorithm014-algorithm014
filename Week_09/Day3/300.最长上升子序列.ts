dbad613a5453e1bc2aeb0f504bf830ce9bdb30bd
xuke <blk1314@126.com>
2020-10-14 16:47:33 +0800

    [300] 最长上升子序列

--- Week_09/Day3/300.最长上升子序列.ts
+++ Week_09/Day3/300.最长上升子序列.ts
@@ -6,7 +6,23 @@
 
 // @lc code=start
 function lengthOfLIS(nums: number[]): number {
-
+    let len = nums.length;
+    if (len === 0) {
+        return 0;
+    }
+    let dp: number[] = [];
+    dp[0] = 1;
+    let res = dp[0];
+    for (let i = 1; i < len; i++) {
+        dp[i] = 1;
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+        res = Math.max(res, dp[i]);
+    }
+    return res;
 };
 // @lc code=end
 
