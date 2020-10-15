e89e739e3a226270f1e86c134efbc1f97b17b7a0
xuke <blk1314@126.com>
2020-10-15 17:20:37 +0800

    [680] 验证回文字符串 Ⅱ

--- Week_09/Day4/680.验证回文字符串-ⅱ.ts
+++ Week_09/Day4/680.验证回文字符串-ⅱ.ts
@@ -6,7 +6,32 @@
 
 // @lc code=start
 function validPalindrome(s: string): boolean {
-
+    let len = s.length;
+    let left = 0;
+    let right = len - 1;
+    while (left < right) {
+        if (s[left] === s[right]) {
+            left++;
+            right--;
+        } else {
+            let flagL = true;
+            let flagR = true;
+            for (let i = left + 1, j = right; i < j; i++, j--) {
+                if (s[i] !== s[j]) {
+                    flagL = false;
+                    break;
+                }
+            }
+            for (let i = left, j = right - 1; i < j; i++, j--) {
+                if (s[i] !== s[j]) {
+                    flagR = false;
+                    break;
+                }  
+            }
+            return flagL || flagR;
+        }
+    }
+    return true;
 };
 // @lc code=end
 
