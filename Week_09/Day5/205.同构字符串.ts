045f3a11d427660ee0c28259340a4e203e5bc918
xuke <blk1314@126.com>
2020-10-16 12:23:04 +0800

    [205] 同构字符串

--- Week_09/Day5/205.同构字符串.ts
+++ Week_09/Day5/205.同构字符串.ts
@@ -6,7 +6,14 @@
 
 // @lc code=start
 function isIsomorphic(s: string, t: string): boolean {
-
+    let sa = s.split('');
+    let ta = t.split('');
+    for (let i = 0; i < sa.length; i++) {
+        if (sa.indexOf(s[i]) !== ta.indexOf(t[i])) {
+            return false;
+        }
+    }
+    return true;
 };
 // @lc code=end
 
