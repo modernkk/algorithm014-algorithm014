6ce8204188309acec62557524335c363444d28d5
xuke <blk1314@126.com>
2020-10-17 10:19:35 +0800

    [32] 最长有效括号

--- Week_09/Day6/32.最长有效括号.ts
+++ Week_09/Day6/32.最长有效括号.ts
@@ -6,7 +6,20 @@
 
 // @lc code=start
 function longestValidParentheses(s: string): number {
-
+    let maxans = 0;
+    let len = s.length;
+    let dp: number[] = new Array(len).fill(0);
+    for (let i = 1; i < len; i++) {
+        if (s[i] === ')') {
+            if (s[i - 1] === '(') {
+                dp[i] = (i > 2 ? dp[i - 2] : 0) + 2;
+            } else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] === '(') {
+                dp[i] = dp[i - 1] + (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+            }
+        }
+        maxans = Math.max(dp[i], maxans);
+    }
+    return maxans;
 };
 // @lc code=end
 
